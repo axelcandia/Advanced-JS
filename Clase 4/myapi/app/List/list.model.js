35cc503ef1c066768c4401b3ac762fa67f6b42f2
Invitado <invitado@MacBook-Pro-de-mac.local>
2018-07-26 16:26:03 -0300

    pdf

--- Clase 4/myapi/app/List/list.model.js
+++ Clase 4/myapi/app/List/list.model.js
@@ -4,9 +4,14 @@ const ObjectId = Schema.ObjectId;
  
 const BlogPost = new Schema({
   author: ObjectId,
-  title: String,
+  task: [{
+    type:ObjectId,
+    ref:"Task"
+  }],
   body: String,
   date: Date
 });
 
 module.exports= mongoose.model("blogSpot",BlogPost);
+
+
